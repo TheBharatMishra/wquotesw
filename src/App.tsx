f7597321f754f6e276ccfc905d36a9467eccaf95
TheBharatMishra <mishrabm2004@gmail.com>
2023-08-27 09:35:58 +0530

    Done for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import bb from "./assets/bb.png";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const initQuote = { quote: "I am Skyler White, Yo!", author: "Skyler White" };
+  const [quote, setQuote] = useState(initQuote.quote);
+  const [author, setAuthor] = useState(initQuote.author);
+  const getQuote = () => {
+    axios
+      .get("https://api.breakingbadquotes.xyz/v1/quotes/")
+      .then((res: any) => {
+        setQuote(res.data[0].quote);
+        setAuthor(res.data[0].author);
+      });
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <main className="h-screen font-mono border-yellow-600 bg-green-900 border-8 flex items-center justify-center flex-col">
+        <img className="h-20" src={bb} alt="breaking bad" />
+        <div className="m-5 text-center">
+          <h1 className="text-white text-2xl m-5">{quote}</h1>
+          <h1 className="text-black text-4xl">~{author}</h1>
+        </div>
+        <button
+          className="rounded font-bold bg-yellow-600 p-2"
+          onClick={getQuote}
+        >
+          Next Quote
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
